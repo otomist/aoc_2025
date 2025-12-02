cf95ee7ed063211728afb31b8ce3a9c96c3090fc
otomist <13265059+otomist@users.noreply.github.com>
2025-12-02 14:38:05 -0500

    p2

--- day2/p1.py
+++ day2/p1.py
@@ -14,15 +14,20 @@ def invalid_p1(num):
     return False
 
 
+def invalid_window(s, ws, max_ws):
+    s1 = s[:ws]
+    for curr in range(0, len(s), ws):
+        s2 = s[curr:curr+ws]
+        if s1 != s2:
+            return False
+    return True
+
 def invalid_p2(num):
     s = str(num)
-    if len(s) % 2 != 0:
-        return False
-    
-    window_size = len(s) // 2
-    if s[:window_size] == s[window_size:]:
-        return True
-
+    max_window_size = len(s) // 2
+    for ws in range(1, max_window_size + 1):
+        if invalid_window(s, ws, max_window_size):
+            return True
     return False
 
 fname = "data.txt"
